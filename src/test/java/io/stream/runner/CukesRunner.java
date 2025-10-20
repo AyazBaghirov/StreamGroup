a27e82897f987a359485bb6c3da0d6f9281ecb78
Admin <ayazbagirov89@gmail.com>
2025-10-20 10:56:31 -0400

    first commit

---
+++ src/test/java/io/stream/runner/CukesRunner.java
@@ -0,0 +1,73 @@
+package io.stream.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/html-reports/cucumber-report.html",
+                  "json:target/json-reports/json-report.json",
+                  "rerun:target/rerun.txt",
+                   "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        features = "src/test/resources/features",
+        glue = "io/stream/step_definitions",
+        dryRun = false,
+        tags = "@smoke",
+        monochrome = true,
+        publish = false
+)
+
+/*
+junit 5 version
+@Suite
+@SelectClasspathResource("features")   // looks under src/test/resources/features
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io/loop/step_definitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "html:target/html-reports/cucumber-report.html, json:target/json-reports/json-report.json")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@listOfMap")
+@ConfigurationParameter(key = MONOCHROME_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "false")
+@ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
+ */
+
+public class CukesRunner {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/io/stream/utilities/Driver.java
@@ -0,0 +1,94 @@
+package io.stream.utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    /*
+    Creating a private constructor, so we are closing access to the object of this class from outside the class
+     */
+
+    private Driver(){
+
+    }
+
+    /*
+    Making our driver instance private, so it is not reachable from outside the class
+    We make it static because we want it to run before everything else, and we will use it in a static method
+     */
+
+    // private static WebDriver driver;
+    // implement threadLocal to achieve multi thread locally
+     private static InheritableThreadLocal <WebDriver> driverPool = new InheritableThreadLocal<>();
+
+    /*
+    Creating a reusable method that will return the same driver instance every time when we call it
+     */
+
+    /**
+     * Singleton pattern
+     * @return
+     */
+    public static WebDriver getDriver() {
+        if (driverPool.get() == null) {
+            String browserType = ConfigurationReader.getProperties("browser");
+            ChromeOptions options = new ChromeOptions();
+            switch (browserType.toLowerCase()) {
+                case "chrome" -> {
+                    options.addArguments("--disable-blink-features=AutomationControlled");
+//                    options.setExperimentalOption("prefs", new java.util.HashMap<String, Object>() {{
+//                        put("credentials_enable_service", false);
+//                        put("profile.password_manager_enabled", false);
+//                        put("profile.password_manager_leak_detection", false);
+//                        put("autofill.profile_enabled", false);
+//                        put("autofill.credit_card_enabled", false);
+//                        options.addArguments("--disable-blink-features=AutomationControlled");
+//                        options.addArguments("--disable-blink-features=AutomationControlled");
+//                        options.addArguments("--disable-password-manager-reauthentication");
+//                        options.addArguments("--disable-features=PasswordLeakDetection,PasswordManagerOnboarding");
+//                    }});
+                    driverPool.set(new ChromeDriver(options));
+                }
+                case "firefox" -> driverPool.set(new FirefoxDriver());
+                case "safari" -> driverPool.set(new SafariDriver());
+                case "headless" -> {
+                    options.addArguments("--disable-blink-features=AutomationControlled");
+                    options.addArguments("--headless");
+                    driverPool.set(new ChromeDriver(options));
+                }
+
+            }
+            driverPool.get().manage().window().maximize();
+            driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+        return driverPool.get();
+    }
+
+    /**
+     * Closing the driver
+     * @author nsh
+     */
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
+            driverPool.get().quit();
+            driverPool.remove();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
